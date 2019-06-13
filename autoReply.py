469d74ca8c903fff3563d00e04019a3aed754c5b
os <542413041@qq.com>
2019-06-13 09:29:20 +0800

    Update autoReply.py

--- autoReply.py
+++ autoReply.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python3
 # coding: utf-8
 from wxpy import *
+import time
+
+SourceSavePath = '.\\ReceiveFile\\'
+
 bot = Bot()
-tuling = Tuling(api_key='你的key')
-@bot.register(msg_types=TEXT)
+tuling = Tuling(api_key='')
+
+@bot.register()
 def auto_reply_all(msg):
+
+    print(msg)
     if isinstance(msg.chat, Group) and not msg.is_at:
-        return
+      return
+    elif msg.type == 'Text':
+      time.sleep(2)
+      tuling.do_reply(msg)
+    elif msg.type == 'Picture':
+      savePath = SourceSavePath+msg.file_name
+      msg.get_file(savePath)
+      msg.reply_image(savePath)
     else:
-        tuling.do_reply(msg)
+      msg.forward(msg.sender)
 bot.join()
